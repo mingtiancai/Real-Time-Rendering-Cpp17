d6f422b78656e4788ae54900d9cf245f9f6fec83
mingtiancai <2427373908@qq.com>
2022-06-07 14:49:54 +0800

    更新背景颜色

--- src/examples/learnOpenGL/chapter1/shadersClass/main.cpp
+++ src/examples/learnOpenGL/chapter1/shadersClass/main.cpp
@@ -202,7 +202,7 @@ int main()
 
         // render
         // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // render the triangle
