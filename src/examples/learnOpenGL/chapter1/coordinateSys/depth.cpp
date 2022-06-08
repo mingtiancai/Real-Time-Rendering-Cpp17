e673a6dcf50b68074a23ba7e5e8150305ab297a1
mingtiancai <2427373908@qq.com>
2022-06-08 18:01:33 +0800

    删除一个面

--- src/examples/learnOpenGL/chapter1/coordinateSys/depth.cpp
+++ src/examples/learnOpenGL/chapter1/coordinateSys/depth.cpp
@@ -89,12 +89,12 @@ int main()
         0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
         0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
 
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-        0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
-        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        // -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        // 0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+        // 0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        // 0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        // -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        // -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
 
         -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
         0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
@@ -201,7 +201,7 @@ int main()
         glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.6f, 0.0f, 0.0f));
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         // retrieve the matrix uniform locations
